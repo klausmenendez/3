aaf8de799831015755613eabd124c7fc0f0d511e
klausmenendez <menendek@oregonstate.edu>
2022-10-05 11:40:18 -0700

    commit

--- temp_convert.py
+++ temp_convert.py
@@ -1,6 +1,6 @@
 print("Please enter a Celsius Temperature")
 
 Celsius=float(input())
-Farenheit=((9/5)*Celsius)+32
-print("The equivelant Farenheit temperature is: ")
-print(Farenheit)
+Fahrenheit=((9/5)*Celsius)+32
+print("The equivelant Fahrenheit temperature is:")
+print(Fahrenheit)
